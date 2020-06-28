46f8c37fb98b16532b2811500ae4255b82004866
Andrey Mukhin <muhinav14@mail.ru>
2020-06-28 16:15:14 +0300

    Add currency for euro and dollar. Add buttons for it.

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ import re
 from bs4 import BeautifulSoup
 from telebot import types
 
-TG_TOKEN = '1152544884:AAEtSB-YpxGFgsWXcrTCIxrFe1PiJXYVpMo'
-
 
 class RateProcessing:
 
     def __init__(self):
-        self.dollar_ruble_ref = 'https://www.google.com/search?q=%D0%BA%D1%83%D1%80%D1%81+%D0%B4%D0%BE%D0%BB%D0%BB%' \
-                                'D0%B0%D1%80%D0%B0&oq=rehc&aqs=chrome.1.69i57j0l7.2693j0j8&sourceid=chrome&ie=UTF-8'
-        self.euro_ruble_ref = 'https://www.google.com/search?ei=CHr2XtuzO6LprgTN0omIAg&q=euro&oq=%D1%83%D0%B3%D0%BA%' \
-                              'D1%89&gs_lcp=CgZwc3ktYWIQARgDMggIABAKEAEQKjIGCAAQChABMgYIABAKEAEyBggAEAoQATIGCAAQChAB' \
-                              'MgYIABAKEAEyBggAEAoQATIGCAAQChABMgYIABAKEAEyBggAEAoQAToECAAQRzoHCAAQsQMQQzoFCAAQsQM6A' \
-                              'ggAOgQIABBDOgwIABCxAxBDEEYQggI6BQgAEIMBUOcaWO8_YLJbaAFwAXgAgAF7iAHzBZIBBDExLjGYAQCgA' \
-                              'QGqAQdnd3Mtd2l6sAEA&sclient=psy-ab'
+        self.currencies_ref_dict = {
+            'EUR': 'https://www.google.com/search?ei=CHr2XtuzO6LprgTN0omIAg&q=euro&oq=%D1%83%D0%B3%D0%BA%D1%89'
+                   '&gs_lcp=CgZwc3ktYWIQARgDMggIABAKEAEQKjIGCAAQChABMgYIABAKEAEyBggAEAoQATIGCAAQChABMgYIABAKEA'
+                   'EyBggAEAoQATIGCAAQChABMgYIABAKEAEyBggAEAoQAToECAAQRzoHCAAQsQMQQzoFCAAQsQM6AggAOgQIABBDOgwI'
+                   'ABCxAxBDEEYQggI6BQgAEIMBUOcaWO8_YLJbaAFwAXgAgAF7iAHzBZIBBDExLjGYAQCgAQGqAQdnd3Mtd2l6sAEA&'
+                   'sclient=psy-ab',
+            'USD': 'https://www.google.com/search?q=%D0%BA%D1%83%D1%80%D1%81+%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1'
+                   '%80%D0%B0&oq=rehc&aqs=chrome.1.69i57j0l7.2693j0j8&sourceid=chrome&ie=UTF-8'
+        }
+        self.currencies_ref_dict_key = 'USD'
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) '
                           'Chrome/83.0.4103.106 Safari/537.36'
@@ -24,8 +25,11 @@ class RateProcessing:
         self.rate = 0.0000
         self.round_rate = 0.00
 
+    def get_currency_ref(self):
+        return self.currencies_ref_dict[self.currencies_ref_dict_key]
+
     def parse_html(self):
-        full_page = requests.get(self.dollar_ruble_ref, headers=self.headers)
+        full_page = requests.get(self.get_currency_ref(), headers=self.headers)
         soup = BeautifulSoup(full_page.content, 'html.parser')
         convert = soup.find_all('span', {'class': 'DFlfde SwHCTb', 'data-precision': 2})
         return str(convert[0])
@@ -38,24 +42,25 @@ class RateProcessing:
 
 class ExchangeBot:
 
-    def __init__(self, rate_processing):
+    def __init__(self, rate_processing: RateProcessing):
         self.rates_class = rate_processing
+        self.tg_token = '1152544884:AAEtSB-YpxGFgsWXcrTCIxrFe1PiJXYVpMo'
 
     def process_bot(self):
-        bot = telebot.TeleBot(TG_TOKEN)
+        bot = telebot.TeleBot(self.tg_token)
 
         @bot.message_handler(commands=['start'])
         def send_welcome(message):
-            bot.send_message(message.chat.id, "Добро пожаловать, {}!\nЯ - ExchangeBot. Подскажу тебе актуальный курс доллара "
-                                      "и евро на данный момент. Чуть позже у меня добавится возможность предупреждать "
-                                      "тебя о достижении долларом "
-                                      "или евро курса, который ты укажешь сам!".format(message.from_user.first_name))
+            bot.send_message(message.chat.id,
+                             "Добро пожаловать, {}!\nЯ - ExchangeBot. Подскажу тебе актуальный курс доллара "
+                             "и евро на данный момент. Чуть позже у меня добавится возможность предупреждать "
+                             "тебя о достижении долларом "
+                             "или евро курса, который ты укажешь сам!".format(message.from_user.first_name))
 
-            markup = types.ReplyKeyboardMarkup(row_width=2)
-            itembtn1 = types.KeyboardButton('a')
-            itembtn2 = types.KeyboardButton('v')
-            itembtn3 = types.KeyboardButton('d')
-            markup.add(itembtn1, itembtn2, itembtn3)
+            markup = types.ReplyKeyboardMarkup(row_width=1)
+            itembtn1 = types.KeyboardButton('Доллар')
+            itembtn2 = types.KeyboardButton('Евро')
+            markup.add(itembtn1, itembtn2)
             bot.send_message(message.chat.id, "Choose one letter:", reply_markup=markup)
 
         @bot.message_handler(commands=['help'])
@@ -64,9 +69,20 @@ class ExchangeBot:
 
         @bot.message_handler(content_types=['text'])
         def get_text_messages(message):
-            if message.text == "Привет":
+            if message.text == "Евро" or message.text == "евро":
+                self.rates_class.currencies_ref_dict_key = 'EUR'
                 bot.send_message(message.from_user.id,
-                                 "Привет, курс доллара на сегодня = {}.".format(self.rates_class.rate))
+                                 "3 секунды, пожалуйста.", disable_web_page_preview=0, disable_notification=0)
+                self.rates_class.get_rates()
+                bot.send_message(message.from_user.id,
+                                 "Курс евро на сегодня = {} руб.".format(self.rates_class.round_rate))
+            elif message.text == "Доллар" or message.text == "доллар":
+                self.rates_class.currencies_ref_dict_key = 'USD'
+                bot.send_message(message.from_user.id,
+                                 "3 секунды, пожалуйста.")
+                self.rates_class.get_rates()
+                bot.send_message(message.from_user.id,
+                                 "Курс доллара на сегодня = {} руб.".format(self.rates_class.round_rate))
             else:
                 bot.send_message(message.chat.id, "Я тебя не понимаю. Напиши /help.")
 
@@ -75,7 +91,6 @@ class ExchangeBot:
 
 def main():
     rate_processing = RateProcessing()
-    rate_processing.get_rates()
     exchange_bot = ExchangeBot(rate_processing)
     exchange_bot.process_bot()
 

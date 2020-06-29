1768b6e0d2cb9fa1746314381ee782f57a14989e
Andrey Mukhin <muhinav14@mail.ru>
2020-06-30 01:03:00 +0300

    Code optimization.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import time
 import telebot
 import requests
 import re
+from datetime import datetime
 from bs4 import BeautifulSoup
 from telebot import types
 
@@ -45,54 +47,68 @@ class ExchangeBot:
     def __init__(self, rate_processing: RateProcessing):
         self.rates_class = rate_processing
         self.tg_token = '1152544884:AAEtSB-YpxGFgsWXcrTCIxrFe1PiJXYVpMo'
+        self.bot = telebot.TeleBot(self.tg_token)
+        self.currency_variety_dict = {
+            "Евро": ["EUR", "€"],
+            "евро": ["EUR", "€"],
+            "Доллар": ["USD", "$"],
+            "доллар": ["USD", "$"],
+        }
 
-    def process_bot(self):
-        bot = telebot.TeleBot(self.tg_token)
-
-        @bot.message_handler(commands=['start'])
+    def welcome_user(self):
+        @self.bot.message_handler(commands=['start'])
         def send_welcome(message):
-            bot.send_message(message.chat.id,
-                             "Добро пожаловать, {}!\nЯ - ExchangeBot. Подскажу тебе актуальный курс доллара "
-                             "и евро на данный момент. Чуть позже у меня добавится возможность предупреждать "
-                             "тебя о достижении долларом "
-                             "или евро курса, который ты укажешь сам!".format(message.from_user.first_name))
+            self.bot.send_message(message.chat.id,
+                                  "Добро пожаловать, {}!\nЯ - ExchangeBot. Подскажу тебе актуальный курс доллара "
+                                  "и евро на данный момент. Чуть позже у меня добавится возможность предупреждать "
+                                  "тебя о достижении долларом "
+                                  "или евро курса, который ты укажешь сам!".format(message.from_user.first_name))
 
-            markup = types.ReplyKeyboardMarkup(row_width=1)
-            itembtn1 = types.KeyboardButton('Доллар')
-            itembtn2 = types.KeyboardButton('Евро')
-            markup.add(itembtn1, itembtn2)
-            bot.send_message(message.chat.id, "Choose one letter:", reply_markup=markup)
+            main_menu = types.ReplyKeyboardMarkup(row_width=1)
+            item_btn1 = types.KeyboardButton('Узнать курсы валют')
+            item_btn2 = types.KeyboardButton('Установить уровень валюты')
+            main_menu.add(item_btn1, item_btn2)
+            self.bot.send_message(message.chat.id, "Выберите нужную опцию:", reply_markup=main_menu)
 
-        @bot.message_handler(commands=['help'])
+    def help_user(self):
+        @self.bot.message_handler(commands=['help'])
         def send_welcome(message):
-            bot.reply_to(message, "Для получения актуального курса доллара напиши Привет")
+            self.bot.reply_to(message, "Для получения актуального курса доллара напиши Привет")
 
-        @bot.message_handler(content_types=['text'])
+    def process_user_request(self):
+        pass
+
+    def process_rate_exchange(self):
+        @self.bot.message_handler(content_types=['text'])
         def get_text_messages(message):
-            if message.text == "Евро" or message.text == "евро":
-                self.rates_class.currencies_ref_dict_key = 'EUR'
-                bot.send_message(message.from_user.id,
-                                 "3 секунды, пожалуйста.", disable_web_page_preview=0, disable_notification=0)
+            if message.text in list(self.currency_variety_dict.keys()):
+                self.rates_class.currencies_ref_dict_key = self.currency_variety_dict[message.text][0]
+                self.bot.send_message(message.from_user.id,
+                                      "3 секунды, пожалуйста.", disable_web_page_preview=0, disable_notification=0)
                 self.rates_class.get_rates()
-                bot.send_message(message.from_user.id,
-                                 "Курс евро на сегодня = {} руб.".format(self.rates_class.round_rate))
-            elif message.text == "Доллар" or message.text == "доллар":
-                self.rates_class.currencies_ref_dict_key = 'USD'
-                bot.send_message(message.from_user.id,
-                                 "3 секунды, пожалуйста.")
-                self.rates_class.get_rates()
-                bot.send_message(message.from_user.id,
-                                 "Курс доллара на сегодня = {} руб.".format(self.rates_class.round_rate))
+                self.bot.send_message(message.from_user.id,
+                                      "На {}\n1 {} = {} ₽".format(
+                                          time.ctime(time.time()),
+                                          self.currency_variety_dict[message.text][1],
+                                          self.rates_class.round_rate)
+                                      )
             else:
-                bot.send_message(message.chat.id, "Я тебя не понимаю. Напиши /help.")
+                self.bot.send_message(message.chat.id, "Я тебя не понимаю. Напиши /help.")
 
-        bot.polling()
+    def set_currency_level(self):
+        pass
+
+    def execute(self):
+        self.welcome_user()
+        self.help_user()
+        self.process_rate_exchange()
+        self.bot.polling()
 
 
 def main():
     rate_processing = RateProcessing()
     exchange_bot = ExchangeBot(rate_processing)
-    exchange_bot.process_bot()
+    exchange_bot.execute()
 
 
 if __name__ == '__main__':

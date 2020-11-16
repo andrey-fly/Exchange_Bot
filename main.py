854a8b846b9e51e3b2a7b542ff0bd78bc24b4ebd
Andrey Mukhin <mukhin.av.1997@gmail.com>
2020-11-16 14:13:59 +0300

    Heroku app to deploy.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import threading
+
 import telebot
 from flask import Flask, request
 from rate_processing import RateProcessing as RPClass
@@ -10,7 +12,7 @@ ebclass = EBClass(rpclass)
 server = Flask(__name__)
 
 
-@server.route('/' + ebclass.tg_token, methods=['POST'])
+@server.route('/' + ebclass.sec_url, methods=['POST'])
 def getMessage():
     ebclass.bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode('utf-8'))])
     return '!', 200
@@ -18,7 +20,6 @@ def getMessage():
 
 @server.route('/')
 def webhook():
-    main()
     return '!', 200
 
 
@@ -28,4 +29,5 @@ def main():
 
 
 if __name__ == '__main__':
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    threading.Thread(target=server.run, args=("0.0.0.0", int(os.environ.get('PORT', 5000)))).start()
+    main()
